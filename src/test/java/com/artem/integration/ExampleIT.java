335283202928b1dcd49c464335014a975c3c3969
Artem Andriushchenko <link.by.artem@gmail.com>
2023-06-27 04:03:48 +0300

    add docker-compose.yml

---
+++ src/test/java/com/artem/integration/ExampleIT.java
@@ -0,0 +1,12 @@
+package com.artem.integration;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExampleIT {
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
